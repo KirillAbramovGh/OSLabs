87d7f247a00170f145a8404631530d9110348ac1
Kirill <8>
2020-02-17 17:38:47 +0300

    made a second lab

--- main.cpp
+++ main.cpp
@@ -1,44 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main()
-{
-    const int n = 2;
-    const int m = 10;
-    int A[n][m];
 
-    for (int i = 0; i < n; i++)
+void displayArray(int* A, int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d ", A[i]);
+    }
+}
+
+void inputArray(int A[2][10]) {
+    for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < m; j++) {
-            printf("A[%d][%d] = ", i,j);
+        for (int j = 0; j < 10; j++) {
+            printf("A[%d][%d] = ", i, j);
             scanf("%d", &A[i][j]);
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m;j++) {
-            printf("%d ", A[i][j]);
-        }
-        printf("%s", "\n");
-    }
+}
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m;j++) {
-            for (int k = i; k < n;k++) {
+int* getIndexesEqualElements(int A[2][10]) {
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 10;j++) {
+            for (int k = i; k < 2;k++) {
                 int l;
                 if (k == i) {
-                    l = j+1;
+                    l = j + 1;
                 }
                 else {
                     l = 0;
                 }
-                for (;l < m;l++) {
-                    if (!(i==k && l==j) && A[i][j] == A[k][l]) {
-                        printf("A[%d][%d] = A[%d][%d]", i, j, k, l);
-                        printf("%s", "\n");
+                for (;l < 10;l++) {
+                    if (A[i][j] == A[k][l]) {
+                        return {i, j, k, l};
                     }
                 }
             }
         }
     }
 
+}
+
+int main()
+{
+    int A[2][10];
+
+    inputArray(A);
+    int* res = getIndexesEqualElements(A);
+    displayArray(res,4);
+
     return 0;
 }
+
